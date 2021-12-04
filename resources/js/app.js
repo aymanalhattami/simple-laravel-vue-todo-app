1b63cc71d098fb95d5edb9f92e6295df80a1edb4
aymen-moh <aymen.alyemen@gmail.com>
2021-12-04 23:15:43 +0300

    simple laravel,vue,axios todo app

---
+++ resources/js/app.js
@@ -0,0 +1,20 @@
+require('./bootstrap');
+
+import Vue from 'vue';
+import App from './vue/app';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faPlusSquare, faTrash } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+library.add(faPlusSquare, faTrash)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+Vue.config.productionTip = false
+
+const app = new Vue({
+    el: '#app',
+    components: {App}
+});
